a6f97e57ccbb95a57e51d8dbbfc9552720b761a6
Dennis <realdkk@gmail.com>
2015-10-16 17:37:32 +0200

    A reasonable UI for the start page

--- website/index.php
+++ website/index.php
@@ -1,29 +1,45 @@
 <html lang="en">
-  <head>
+  <head>   
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    
     <title>HashTux</title>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    
+    <link href="css/bootstrap.css" rel="stylesheet">  
   </head>
-    <div class="me">
-      <h1 align="center">Hello HashTux!</h1>
+  
+  <body>
+      
+        <div class="header">
+            <h1 align="center">#hashtux</h1>
+        </div>
+        
+      <div class="container">
 
-      <input type="text" class="form-control" placeholder="#HashTux" align="center">
+          <div class="row">
+              
+              <div class="col-md-4"></div>
+              
+              <div class="col-md-4 col-fill">
+                  
+                    <div class="search">
+                        <p align="center">Please search for a hashtag!</p>
+                        <div class="input-group">
+                            <span class="input-group-addon">#</span>
+                            <input type="text" class="form-control" />
+                        </div>
+                    </div>
+              </div>
+              
+              <div class="col-md-4"></div>
+              
+          </div>
+
     </div>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
