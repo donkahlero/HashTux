50579bb4e439b721503ddaa59b2ca0ece9b9cb87
Niklas le Comte <niklas.lecomte@hotmail.com>
2015-10-19 13:01:36 +0200

    Connector is working for get,put, delete

--- erlang/src/couch_connector.erl
+++ erlang/src/couch_connector.erl
@@ -1,9 +1,10 @@
 %% @author Niklas le Comte niklas.lecomte@hotmail.com [www.hashtux.com/niklas]
-%% %% @doc Initial database actions module
+%% %% @doc CouchDB connector
 %% %% @version 0.1
 
 -module(couch_connector).
 
+%% These are macros for connecting to the DB.
 -define(URI, "derkahler.de").
 -define(PORT, "1994").
 -define(USER, "hashtux").
@@ -13,29 +14,28 @@
 -export([get_info/0, put_request/3, get_request/1, delete_request/1, post_request/3]).
  -version("0.1").
 
+%% @doc This function creates a header for the HTTP.
 auth_header(User, Pass) ->
 	            Encoded = base64:encode_to_string(lists:append([User,":",Pass])),
 		                    {"Authorization","Basic " ++ Encoded}.
-
+%% @doc This function gets the general information from the database.
 get_info() ->
-	get_request([]).
+	?MODULE:get_request([]).
 
+%% @doc This function puts in a new DB or new document as well as adding fields to it.
 put_request(DocAddr, Content, Type) ->
 	Headers = [auth_header(?USER, ?PW), {"Content-Type", Type}],
 	Options = [{body_format, string}],
 	httpc:request(put, {?ADDR ++ DocAddr, Headers, Type, Content}, [], Options).
 
-%% Is not completed
-post_request(DocAddr, Content, Type) ->
-	Headers = [auth_header(?USER, ?PW), {"Content-Type", Type}],
-	Options = [{body_format, binary}],
-	httpc:request(post, {?ADDR ++ DocAddr, Headers, Type, Content}, [], Options).
-
+%% @doc This function gets the infromation from the DB. You can get general information of go deeper
+%% and get specific document and json files.
 get_request(DocAddr) ->
 	Headers = [auth_header(?USER, ?PW)],
 	Options = [{body_format, binary}],
 	httpc:request(get, {?ADDR ++ DocAddr, Headers}, [], Options).
 
+%% @doc This function deletes data from the DB.
 delete_request(DocAddr) ->
 	Headers = [auth_header(?USER, ?PW)],
 	Options = [{body_format, binary}],
