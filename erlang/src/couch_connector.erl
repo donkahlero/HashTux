470b78ebb263c4b720c5539daa12919aa05c9b1e
Niklas le Comte <niklas.lecomte@hotmail.com>
2015-10-19 12:07:04 +0200

    Changed couch_connector so it isn't a gen_server any more

--- erlang/src/couch_connector.erl
+++ erlang/src/couch_connector.erl
@@ -10,19 +10,33 @@
 -define(PW, "grouptux").
 -define(ADDR,"http://" ++ ?URI ++ ":" ++ ?PORT ++ "/").
 
--export([get_info/0, put_request/3]).
+-export([get_info/0, put_request/3, get_request/1, delete_request/1, post_request/3]).
  -version("0.1").
 
+auth_header(User, Pass) ->
+	            Encoded = base64:encode_to_string(lists:append([User,":",Pass])),
+		                    {"Authorization","Basic " ++ Encoded}.
 
 get_info() ->
-	{ok, {{_Version, 200, _ReasonPhrase}, _Headers, Body}} = httpc:request(get, {?ADDR, []}, [], []),
-	Body.
+	get_request([]).
 
 put_request(DocAddr, Content, Type) ->
 	Headers = [auth_header(?USER, ?PW), {"Content-Type", Type}],
-	Options = [{body_format,binary}],
+	Options = [{body_format, string}],
 	httpc:request(put, {?ADDR ++ DocAddr, Headers, Type, Content}, [], Options).
 
-auth_header(User, Pass) ->
-	    Encoded = base64:encode_to_string(lists:append([User,":",Pass])),
-	        {"Authorization","Basic " ++ Encoded}.
+%% Is not completed
+post_request(DocAddr, Content, Type) ->
+	Headers = [auth_header(?USER, ?PW), {"Content-Type", Type}],
+	Options = [{body_format, binary}],
+	httpc:request(post, {?ADDR ++ DocAddr, Headers, Type, Content}, [], Options).
+
+get_request(DocAddr) ->
+	Headers = [auth_header(?USER, ?PW)],
+	Options = [{body_format, binary}],
+	httpc:request(get, {?ADDR ++ DocAddr, Headers}, [], Options).
+
+delete_request(DocAddr) ->
+	Headers = [auth_header(?USER, ?PW)],
+	Options = [{body_format, binary}],
+	httpc:request(delete, {?ADDR ++ DocAddr, Headers}, [], Options).
