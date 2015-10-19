2f640992afa8e349f40f6c947c1a0af6965c1737
TacoVox <jonas.kahler@icloud.com>
2015-10-19 16:44:10 +0200

    Added several low-level database operations

--- erlang/apps/hashtux/src/couch_operations.erl
+++ erlang/apps/hashtux/src/couch_operations.erl
@@ -8,22 +8,61 @@
 -define(DB, "hashtux/").
 
 %% All document operations
--export([add_doc/2, change_doc/2, get_doc/1, delete_doc/1]).
+-export([doc_add/2, doc_change/2, doc_get/1, doc_delete/1, doc_append/2]).
 
 %% @doc Method to add a document to the database
-add_doc(DocName, Content) ->
-    couch_connector:put_request(DocName, Content, "text/json").
+doc_add(DocName, Content) ->
+    couch_connector:put_request(?DB ++ DocName, jsx:encode(Content), "text/json").
 
 %% @doc Change conent of a document
-change_doc(DocName, Content) ->
-    Rev = jsx:decode(?MODULE:get_doc(DocName)).
-    %couch_connector:put_request(DocName, Rev ++ Content, "text/json").
+doc_change(DocName, Content) ->
+    Rev = get_tupp(jsx:decode((?MODULE:get_doc(DocName))), "_rev"),
+    io:format("~p", [binary_to_list(jsx:encode([Rev | Content]))]),
+    couch_connector:put_request(?DB ++ DocName, binary_to_list(jsx:encode([Rev | Content])), "text/json").
 
 %% @doc Get a document from the database
-get_doc(DocName) ->
+doc_get(DocName) ->
     {ok, {_HTTP, _Info, Res}} = couch_connector:get_request(?DB ++ DocName),
     Res.
 
-delete_doc(DocName) ->
-    Rev = ?MODULE:get_doc(DocName),
-    couch_connector:delete_request(DocName ++ "?rev=" ++ Rev).
+%% @doc Delete document from database
+doc_delete(DocName) ->
+    Rev = get_val(jsx:decode((?MODULE:get_doc(DocName))), "_rev"),
+    couch_connector:delete_request(?DB ++ DocName ++ "?rev=" ++ Rev).
+
+%% @doc Insert data to a document
+doc_append(DocName, Content) ->
+    Origin = jsx:decode(doc_get(DocName)),
+    Rev = get_tupp(Origin, "_rev"),
+    Modified = remove_val(Origin, "_rev"),
+    couch_connector:put_request(?DB ++ DocName, binary_to_list(jsx:encode([Rev] ++ Modified ++ Content)), "text/json").
+%% @doc Removes value from document
+remove_val(Origin, Name) ->
+    remove_val(Origin, [] , Name).
+
+%% Creates new list without the data that was removed
+remove_val([], Res, _Name) ->
+    Res;
+remove_val([H|T], Res, Name) ->
+    {Id, _Val} = H,
+    case (binary_to_list(Id)) of
+	Name ->
+	    remove_val(T, Res, Name);
+	_Other ->
+	    remove_val(T, [H | Res], Name)
+    end.
+	    
+%% @doc Gets a tuple with Json file
+get_tupp([H|T], Field) ->
+    {Id, _Val} = H,
+    case (binary_to_list(Id)) of
+	Field ->
+	    H;
+	_Other ->
+	    get_tupp(T, Field)
+    end.
+
+%% @doc Gets the value from the json with that field
+get_val(Json, Field) ->
+    {_ID, Val} = get_tupp(Json ,Field),
+    binary_to_list(Val).
