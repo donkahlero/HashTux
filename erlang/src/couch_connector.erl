02a3365a4942d1e671a6233552a0c03de7644301
Niklas le Comte <niklas.lecomte@hotmail.com>
2015-10-19 10:58:38 +0200

    Changed couch_connector so it isn't a gen_server any more

--- erlang/src/couch_connector.erl
+++ erlang/src/couch_connector.erl
@@ -2,49 +2,27 @@
 %% %% @doc Initial database actions module
 %% %% @version 0.1
 
- -module(couch_connector).
+-module(couch_connector).
 
--export([start_link/0, stop/0]).
--export([get_info/0]).
--export([init/1, terminate/2, handle_call/3, handle_cast/2, handle_info/2, code_change/3]).
+-define(URI, "derkahler.de").
+-define(PORT, "1994").
+-define(USER, "hashtux").
+-define(PW, "grouptux").
+-define(ADDR,"http://" ++ ?URI ++ ":" ++ ?PORT ++ "/").
+
+-export([get_info/0, put_request/3]).
  -version("0.1").
 
--behavior(gen_server).
 
-%%Normal exp
-start_link() ->
-	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
-
-stop() ->
-	        stop(?MODULE).
-stop(Module) ->
-	        gen_server:call(Module, stop).
-
-%%API for users
 get_info() ->
-	        gen_server:call(?MODULE, info).
+	{ok, {{_Version, 200, _ReasonPhrase}, _Headers, Body}} = httpc:request(get, {?ADDR, []}, [], []),
+	Body.
 
-%%Server stuff
-init(_Args) ->
-	URI = "http://www.derkahler.de:1994/",
-	{ok, {URI}}.
+put_request(DocAddr, Content, Type) ->
+	Headers = [auth_header(?USER, ?PW), {"Content-Type", Type}],
+	Options = [{body_format,binary}],
+	httpc:request(put, {?ADDR ++ DocAddr, Headers, Type, Content}, [], Options).
 
-handle_call(stop, _From, State) ->
-	{stop, normal, stopped, State};
-
-handle_call(info, _From, {URI}) ->
-	{ok, {{_Version, 200, _ReasonPhrase}, _Headers, Body}} = httpc:request(get, {URI, []}, [], []),
-	{reply, Body, {URI}}.
-
-handle_cast(_Msg, State) ->
-    {noreply, State + 1}.
-
-handle_info(_Info, State) ->
-    {noreply, State + 1}.
-
-code_change(_OldVsn, State, _Extra) ->
-	    {ok, State}.
-
-terminate(Reason, State) ->
-	{exit, Reason, State}.
-
+auth_header(User, Pass) ->
+	    Encoded = base64:encode_to_string(lists:append([User,":",Pass])),
+	        {"Authorization","Basic " ++ Encoded}.
