9fff1f955efa669d64568e1472ada4afa66ad3a7
tville <jerker@soundandvision.se>
2015-10-27 15:53:57 +0100

    dd

--- erlang/apps/hashtux/src/main_flow.erl
+++ erlang/apps/hashtux/src/main_flow.erl
@@ -17,20 +17,13 @@ handle_call({search, Term}, From, State) ->
 	
 	%MSG = miner_server:search(Term, none),
 	%io:format("MSG: ~p~n", [MSG]),
-	{reply, Ref, State}.
 	
-	%receive 
-		%{Ref, [{<<"error">>,<<"not_found">>},{<<"reason">>,<<"missing">>}]}
-		  
-	%	{Ref, Res} ->
-	%		{reply, Res, State}
-	%	after 1000 ->
-	%		{reply, "DB timeout!", State}
-	%end.
-	
-	%if (Term == "help") ->
-	%	   {reply, "There is no help for a lost soul like yours.", State};
-	%	true ->
-	%		{reply, lists:reverse(Term), State}
-	%end.
+	receive 
+		{Ref, [{<<"error">>,<<"not_found">>},{<<"reason">>,<<"missing">>}]} ->
+			{reply, "Nothing found!", State};
+		{Ref, Res} ->
+			{reply, Res, State}
+		after 1000 ->
+			{reply, "DB timeout!", State}
+	end.
 				
