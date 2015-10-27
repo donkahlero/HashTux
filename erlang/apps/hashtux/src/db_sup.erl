0aa0c0bcf8c66502385261c66fd341b0148b014a
TacoVox <jonas.kahler@icloud.com>
2015-10-27 15:58:44 +0100

    Added the new general module for the workers

--- erlang/apps/hashtux/src/db_sup.erl
+++ erlang/apps/hashtux/src/db_sup.erl
@@ -25,12 +25,12 @@ shell_start() ->
     unlink(Pid).
 
 init([]) ->
-    HashReadSup = {db_hash_read_sup, {db_hash_read_sup, start_link, []},
-		   temporary, 5000, supervisor, [db_hash_read_sup]},
-    HashWriteSup = {db_hash_write_sup, {db_hash_write_sup, start_link, []},
-		    temporary, 5000, supervisor, [db_hash_write_sup]},
-    StatsWriteSup = {db_stats_write_sup, {db_stats_write_sup, start_link, []},
-                    temporary, 5000, supervisor, [db_stats_write_sup]},
+    HashReadSup = {db_hash_read_sup, {db_worker_sup, start_link, [db_hash_read_sup]},
+		   temporary, 5000, supervisor, [db_worker_sup]},
+    HashWriteSup = {db_hash_write_sup, {db_worker_sup, start_link, [db_hash_write_sup]},
+		    temporary, 5000, supervisor, [db_worker_sup]},
+    StatsWriteSup = {db_stats_write_sup, {db_worker_sup, start_link, [db_stats_write_sup]},
+                    temporary, 5000, supervisor, [db_worker_sup]},
     DBServ = {db_serv, {db_serv, start_link, []},
 	      permanent, 5000, worker, [db_serv]},
     {ok, {{one_for_one, 1, 10}, [HashReadSup, HashWriteSup, StatsWriteSup, DBServ]}}.
