ecf09c0c4a86fbd468b16223aa0336f15cde828c
tville <jerker@soundandvision.se>
2015-10-27 16:07:45 +0100

    dd

--- erlang/apps/hashtux/src/main_flow.erl
+++ erlang/apps/hashtux/src/main_flow.erl
@@ -15,26 +15,26 @@ handle_call({search, Term}, From, State) ->
 	io:format("Term: ~p~n", [Term]),
 	
 	% Make a database call for the term
-	%Ref = gen_server:call(db_serv, {get_cont, Term}),
-	%receive 
-	%	{Ref, [{<<"error">>,<<"not_found">>},{<<"reason">>,<<"missing">>}]} ->
-	%		{reply, "Nothing found!", State};
-	%	{Ref, Res} ->
-	%		{reply, Res, State}
-	%	after 1000 ->
-	%		{reply, "DB timeout!", State}
-	%end.
-	
-	% Make a miner call for the term
-	Ref = miner_server:search(Term, none),
+	Ref = gen_server:call(db_serv, {get_cont, Term}),
 	receive 
-		{Ref, X} ->
-			{reply, X, State}
+		{Ref, [{<<"error">>,<<"not_found">>},{<<"reason">>,<<"missing">>}]} ->
+			{reply, "Nothing found!", State};
+		{Ref, Res} ->
+			{reply, Res, State}
 		after 1000 ->
 			{reply, "DB timeout!", State}
-	end,
+	end.
+	
+	% Make a miner call for the term
+	%Ref = miner_server:search(Term, none),
+	%receive 
+	%	{Ref, X} ->
+	%		{reply, X, State}
+	%	after 1000 ->
+	%		{reply, "DB timeout!", State}
+	%end,
 	
 	%io:format("MSG: ~p~n", [X]),
-	{reply, ok, State}.
+	%{reply, ok, State}.
 
 				
