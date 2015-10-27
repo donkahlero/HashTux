9f3e52b77cec9f8ab87abe1695ee2b523348635d
tville <jerker@soundandvision.se>
2015-10-27 15:19:58 +0100

    Fixed bad reply format

--- erlang/apps/hashtux/src/main_flow.erl
+++ erlang/apps/hashtux/src/main_flow.erl
@@ -14,7 +14,7 @@ init(_Args) ->
 handle_call({search, Term}, From, State) -> 
 	io:format("Term: ~p~n", [Term]),
 	Ref = gen_server:call(db_serv, {get_cont, Term}),
-	receive {Ref, Res} -> {reply, Res} end.
+	receive {Ref, Res} -> {reply, Res, State} end.
 	%if (Term == "help") ->
 	%	   {reply, "There is no help for a lost soul like yours.", State};
 	%	true ->
