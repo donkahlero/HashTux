c66edfea63266b3ce85b07c69ce5ac12f1e8f047
TacoVox <jonas.kahler@icloud.com>
2015-10-27 13:49:17 +0100

    Added stats write sup to the main db sup

--- erlang/apps/hashtux/src/db_sup.erl
+++ erlang/apps/hashtux/src/db_sup.erl
@@ -14,9 +14,11 @@ shell_start() ->
 
 init([]) ->
     HashReadSup = {db_hash_read_sup, {db_hash_read_sup, start_link, []},
-	      temporary, 5000, supervisor, [db_hash_read_sup]},
+		   temporary, 5000, supervisor, [db_hash_read_sup]},
     HashWriteSup = {db_hash_write_sup, {db_hash_write_sup, start_link, []},
-	      temporary, 5000, supervisor, [db_hash_write_sup]},
+		    temporary, 5000, supervisor, [db_hash_write_sup]},
+    StatsWriteSup = {db_stats_write_sup, {db_stats_write_sup, start_link, []},
+                    temporary, 5000, supervisor, [db_stats_write_sup]},
     DBServ = {db_serv, {db_serv, start_link, []},
 	      permanent, 5000, worker, [db_serv]},
-    {ok, {{one_for_one, 1, 10}, [HashReadSup, HashWriteSup, DBServ]}}.
+    {ok, {{one_for_one, 1, 10}, [HashReadSup, HashWriteSup, StatsWriteSup, DBServ]}}.
