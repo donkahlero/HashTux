b304220830d32dddef67b610b684c74a6d3b33a8
TacoVox <jonas.kahler@icloud.com>
2015-10-27 15:52:15 +0100

    Made the registering variable

--- erlang/apps/hashtux/src/db_worker_sup.erl
+++ erlang/apps/hashtux/src/db_worker_sup.erl
@@ -2,11 +2,11 @@
 
 -behavior(supervisor).
 
--export([start_link/0]).
+-export([start_link/1]).
 -export([init/1]).
 
-start_link() ->
-        supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link(Supname) ->
+        supervisor:start_link({local, Supname}, ?MODULE, []).
 
 init([]) ->
     {ok, {{one_for_one, 5, 10}, []} }.
