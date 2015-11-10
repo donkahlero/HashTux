9ecb58ea2fe5c2ce4c0d1329db018bbc1e76383a
TacoVox <jonas.kahler@icloud.com>
2015-11-10 13:50:30 +0100

    Fixed the content type filter

--- erlang/apps/hashtux/src/db_filter.erl
+++ erlang/apps/hashtux/src/db_filter.erl
@@ -2,16 +2,25 @@
 
 -export([content_type/2]).
 
-content_type(L, ContentType) ->
-    content_type(L, ContentType, []).
+content_type(L, CTypes) ->
+    content_type(L, CTypes, []).
 
-content_type([], _ContentType, Res) ->
+content_type([], _CTypes, Res) ->
     Res;
-content_type([X|Xs], ContentType, Res) ->
-    BinType = binary:list_to_bin(ContentType),
-    Type = lists:keyfind(<<"content_type">>, 1, X),
-    case (Type) of
-	BinType ->
-	    content_type(Xs, ContentType, [X|Res]);
-	_ -> content_type(Xs, ContentType, Res)
+content_type([X|Xs], CTypes, Res) ->
+    {_, Type} = lists:keyfind(<<"content_type">>, 1, X),
+    case (is_ctype(CTypes, Type)) of
+	false ->
+	    content_type(Xs, CTypes, Res);
+	true ->
+	    content_type(Xs, CTypes, [X|Res])
+    end.
+    
+is_ctype([], _Type) ->
+    false;
+is_ctype([X|Xs], Type) ->
+    CType = binary:list_to_bin(atom_to_list(X)),
+    case(Type) of
+	CType -> true;
+	_ -> is_ctype(Xs, Type)
     end.
