032f12bb598c4857c3a544dcf4ae948153d7b0d1
tville <jerker@soundandvision.se>
2015-11-10 15:00:02 +0100

    Option parsing module started

--- erlang/apps/hashtux/src/http_handler.erl
+++ erlang/apps/hashtux/src/http_handler.erl
@@ -1,6 +1,8 @@
 %% @author jerker
 %% @doc 
 %
+% This module handles requests from the Apache server handling the frontend.
+%
 % The PHP request supplies a number of details about
 % the user, session and client options.
 % User habit related:
@@ -14,11 +16,16 @@
 %	Services (t, i, y) (also stored as user habit right now)
 %	Resolution (high, low, default)
 %
-
+% Other notes:
+% URL: the full url, including http://
+% {URL, _} =  cowboy_req:url(Req),	
+% QueryString: all the query stuff after the ?
+% {Qs, _} = cowboy_req:qs(Req),
+%
 % 	TODO: Create a function that takes req as an argument
 %	and returns an option tuple to be sent to main flow
 % 	TODO: Handle resolution, with default value handled
-%	TODO: check cURL timeout / load balancing
+%	TODO: check cURL timeout / load balancing on PHP
 %	TODO: Check spaces in search terms
 %	TODO: rememeber client side option text above pic on/off - unrelated here but putting it down not to forget	
 	
@@ -42,13 +49,8 @@ init(_Type, Req, []) ->
 handle(Req, State) ->
 	% Path: the path, starting with /
 	{Path, _} = cowboy_req:path(Req),
-	
-	% URL: the full url, including http://
-	%{URL, _} =  cowboy_req:url(Req),	
-	% QueryString: all the query stuff after the ?
-	%{Qs, _} = cowboy_req:qs(Req),
-		
-	% Extract the path (the search term)
+			
+	% Extract the path (the search term, in our protocol)
 	[_ | Term] = binary:bin_to_list(Path),
 	
 	% "Debug" output
@@ -56,35 +58,24 @@ handle(Req, State) ->
 	
 	% Store user habit data 	
 	user_habits:store(Term, Req),
+	% Extract options
+	Options = http_option_parser:parse_options(Req),
 	
-	% Send the search term and the options to the main flow 
-	%Reply = try gen_server:call(main_flow, {search, Term}, 20000)
-	%	catch _ -> []
-	%end,
-
-	% Make a call to main flow server - get the PID of the worker back
-	% and wait for a reply from it
-
-	{ok, HandlerPid} = gen_server:call(main_flow_server, {search, Term, none}),
+	% Send the search term and the options to the main flow by making a call to 
+	% main flow server - get the PID of the worker back and wait for a reply from it
+	{ok, HandlerPid} = gen_server:call(main_flow_server, {search, Term, Options}),
 	io:format("~nWorker PID: ~p~n", [HandlerPid]),
 	
 	receive 
 		{HandlerPid, Reply} -> 
-			io:format("~nReceved from worker~n", []),
-			ok;
-		X -> 
-			io:format("~nReceved from ? ~p~n", [X]),
-			Reply = []
+			io:format("~nReceved a reply from worker ~p~n", [HandlerPid]),
+			ok
 		after 20000 ->
-			io:format("~nTimeout from worker~n", []),
+			io:format("~nTimeout from worker~p~n", [HandlerPid]),
 			Reply = []
 		end,
 	
 	io:format("Main flow returned from handling ~p~n", [Term]),
-	
-	% io_lib:format does about the same thing as io:format but returns a string instead of printing
-	%Body = io_lib:format("Welcome to HashTux!~n~nHashtag for mining: ~p~nSessionID: ~p~nIP Address: ~p~nLanguage: ~p~nUser Agent: ~p~nResult: ~p~n~n",
-	%					 [Term, SessionID, IPAddress, Language, UserAgent, Reply]),	
 	
 	% Send the reply from the main flow call, which should be
 	% a list of social media posts. We encode it with jsx and send it out.
