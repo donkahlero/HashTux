fa476c0772f1b8ef420dc2a32364bc213163a0ea
ThreePhase3fo <marco.trifance@gmail.com>
2015-11-16 13:59:48 +0100

    Implemented Youtube  advanced search. Refactored Twitter search and parser.

--- erlang/apps/hashtux/src/parser.erl
+++ erlang/apps/hashtux/src/parser.erl
@@ -1,8 +1,8 @@
 -module(parser).
 
 -export([extract/2, atom_to_binarystring/1]).
--export([extract_youtube_ids/1, parse_youtube_video/2, clean_result/1]).									% YOUTUBE Functions
--export([is_content_type/2]).																				% TWITTER Functions
+-export([extract_youtube_ids/1, parse_youtube_video/2, clean_result/1, is_language/2]).						% YOUTUBE Functions
+-export([is_content_type/2, parse_tweet_response_body/2]).																				% TWITTER Functions
 
 % ****************************************
 % @doc A parser for decoded JSON Items
@@ -62,6 +62,8 @@ parse_youtube_video(Video, HashTag) ->
 			PubDate = dateconv:youtube_to_epoch(binary_to_list(extract_from_node(<<"publishedAt">>, Snippet))),		%% convert to EPOCH
 
 			Description = extract_from_node(<<"description">>, Snippet),
+			
+			Language = extract_from_node(<<"defaultAudioLanguage">>, Snippet),
 
 			Tags = extract_from_node(<<"tags">>, Snippet),
 
@@ -78,6 +80,7 @@ parse_youtube_video(Video, HashTag) ->
 			Id = null,
 			PubDate = null,
 			Description = null,
+			Language = null,
 			ViewCount = null,
 			LikeCount = null,
 			Tags = null,
@@ -86,12 +89,33 @@ parse_youtube_video(Video, HashTag) ->
     end,
 
     % NOTE: add 'Clean Result'
-    A = [{<<"search_term">>, list_to_binary(HashTag)}, {<<"service">>, <<"youtube">>}, {<<"insert_timestamp">>, Timestamp}, {<<"timestamp">>, PubDate}, {<<"content_type">>, <<"video">>}, {<<"service_id">>, Id}, {<<"text">>, Description}, {<<"view_count">>, ViewCount}, {<<"likes">>, LikeCount}, {<<"tags">>, Tags}],
+    A = [{<<"search_term">>, list_to_binary(HashTag)}, {<<"service">>, <<"youtube">>}, {<<"insert_timestamp">>, Timestamp}, {<<"timestamp">>, PubDate}, {<<"content_type">>, <<"video">>}, {<<"service_id">>, Id}, {<<"text">>, Description}, {<<"language">>, Language}, {<<"view_count">>, ViewCount}, {<<"likes">>, LikeCount}, {<<"tags">>, Tags}],
 
     % return clean result
     clean_result(A).
 
 % ****************************************
+% @doc Tells if a given Youtube video matches the Language Filter
+is_language(Status, LangFilter) ->
+	
+	case extract_from_node(<<"language">>, Status) of
+		null -> 
+			io:format("EXTRACTED LANG IS NULL ~n"),
+			true;		%% If doc has no Language node, return true
+		
+		Lang -> 
+
+			Extracted_Lang = binary_to_list(Lang),
+
+			io:format("EXTRACTED LANG IS ~p~n", [Extracted_Lang]),
+			io:format("LANG FILTER IS ~p~n", [LangFilter]),
+
+			A = binary_to_list(LangFilter) == Extracted_Lang,
+			
+			io:format("RETURNING ~p~n", [A]),	
+
+			A
+	end.
 
 % @doc Convert an atom to binary_string
 atom_to_binarystring(Atom) ->
@@ -100,6 +124,143 @@ atom_to_binarystring(Atom) ->
 % ****************************************
 % PARSE TWITTER decoded JSON
 % ****************************************
+
+% Decode Response Body and parses result
+parse_tweet_response_body(HashTag, DecodedBody) ->
+
+    %% Debug search Metadata
+    case extract(<<"search_metadata">>, DecodedBody) of
+        {found, SearchMetadata} -> 
+            case extract(<<"count">>, SearchMetadata) of
+                {found, SearchCount} -> io:format("Twitter: fetched ~p feeds from Twitter API ~n", [SearchCount]);
+                not_found -> io:format("Search Count NOT FOUND\n")
+            end;
+
+        not_found -> io:format("Search Metadata NOT FOUND\n")
+    end,
+
+    %% Parse the list of Tweets returned by the Twitter API
+    case extract(<<"statuses">>, DecodedBody) of
+        {found, StatusList} -> 
+            parse_tweet_status_list(HashTag, StatusList, []);                                
+        not_found -> []                                         %% return empty list if result was empty
+    end.
+
+%% PARSING!!!!
+%% Calls parse_status_details on all statuses included in a given List
+%% Sends the document to DB and returns it
+parse_tweet_status_list(_HashTag, [], Result) -> Result;                        %% return doc
+parse_tweet_status_list(HashTag, [H|T], Result) -> 
+    NewResult = Result ++ [parse_tweet_details(HashTag, H)],
+    parse_tweet_status_list(HashTag, T, NewResult).
+
+%% Convert single Tweet Object to internal representation form
+parse_tweet_details(HashTag, Status) ->
+
+    %% Register the time the document was sent to DB
+    Timestamp = dateconv:get_timestamp(),
+
+    %% ======== Parsing Tweet details ==============
+    Tweet_ID = case extract(<<"id">>, Status) of
+        {found, X1} -> integer_to_binary(X1);
+        not_found -> null
+    end,
+
+    Date = case extract(<<"created_at">>, Status) of
+        {found, X2} -> dateconv:twitter_to_epoch(binary_to_list(X2));
+        not_found -> null
+    end,
+
+    Text = extract_from_node(<<"text">>, Status),
+    
+    MetaData = extract_from_node(<<"metadata">>, Status),
+
+    Language = extract_from_node(<<"iso_language_code">>, MetaData),
+
+    Coordinates = extract_from_node(<<"coordinates">>, Status),
+
+	Retweet_Count = extract_from_node(<<"retweet_count">>, Status),
+
+	Favorited = extract_from_node(<<"favorited">>, Status),
+
+    %% Parse 'entities' node in Tweet Object
+    case extract(<<"entities">>, Status) of
+        {found, Entities} ->
+            Tags = case extract(<<"hashtags">>, Entities) of
+                {found, []} -> null;
+                {found, X8} -> format_tweet_tags(X8);
+                not_found -> null
+            end,
+
+            % Parse 'Media' entity
+            case extract(<<"media">>, Entities) of
+                {found, Media} -> 
+                    {Media_URL, Media_Type} = format_media_entity(Media);
+                not_found -> 
+                    Media_URL = null,
+                    Media_Type = <<"text">>
+            end;
+
+        %% ENTITIES not found
+        not_found -> 
+            Tags = null,
+            Media_URL = null,
+            Media_Type = <<"text">>
+    end,
+
+    %% ======== Parsing User details ==============
+
+    UserInfo = extract_from_node(<<"user">>, Status),
+    UserName = extract_from_node(<<"name">>, UserInfo),
+    ScreenName = extract_from_node(<<"screen_name">>, UserInfo),
+    User_Profile_Link = build_tweet_profile_link(ScreenName),
+    UserID = convert_user_ID(extract_from_node(<<"id">>, UserInfo)),
+
+    A = [{<<"search_term">>, list_to_binary(HashTag)},{<<"service">>, <<"twitter">>}, {<<"service_id">>, Tweet_ID}, {<<"timestamp">>, Date}, {<<"insert_timestamp">>, Timestamp}, {<<"text">>, Text}, {<<"language">>, Language}, {<<"view_count">>, Retweet_Count}, {<<"likes">>, Favorited}, {<<"location">>, Coordinates}, {<<"tags">>, Tags}, {<<"resource_link_high">>, Media_URL}, {<<"resource_link_low">>, Media_URL}, {<<"content_type">>, Media_Type}, {<<"free_text_name">>, UserName}, {<<"username">>, ScreenName}, {<<"profile_link">>, User_Profile_Link}, {<<"user_id">>, UserID}],
+    clean_result(A).
+
+% Convert int to binary. Return null if Id is null
+convert_user_ID(null) -> null;
+convert_user_ID(Id) -> integer_to_binary(Id).
+
+% Format decoded Tweet Tags node 
+format_tweet_tags(Tags_List) -> 
+    format_tweet_tags(Tags_List, []).
+
+format_tweet_tags([], Result) -> Result;
+format_tweet_tags([H|T], Result) -> 
+    NewResult = Result ++ [get_single_tag(H)],
+    format_tweet_tags(T, NewResult).
+
+get_single_tag([{<<"text">>, A}, _]) -> A;
+get_single_tag([{_B, A}, _]) -> A.
+
+% Builds Profile URL for a given user screen_name
+build_tweet_profile_link(null) -> null;
+build_tweet_profile_link(Screen_Name) -> 
+    A = lists:append("https://twitter.com/", binary_to_list(Screen_Name)),
+    list_to_binary(A).
+
+% Returns ONLY the first media element information (URL and Type).
+format_media_entity([]) -> {null, null};
+format_media_entity([H|_T]) -> format_single_media(H).          %% return info related to first media element
+
+format_single_media(Media) ->
+    Media_Url = case extract (<<"media_url">>, Media) of
+        {found, Url} -> Url;
+        not_found -> null
+    end,
+
+    Media_Type = case extract (<<"type">>, Media) of
+        {found, Type} -> 
+            case Type of
+                <<"photo">> -> <<"image">>;
+                _ -> <<"video">>
+            end;
+        not_found -> null
+    end,
+
+    {Media_Url, Media_Type}.
 
 % @doc Tells if a given Tweet feed is of one of the types (content-types: text, image, video) included in the list TypeFilter.
 is_content_type(Status, TypeFilter) -> 
--- erlang/apps/hashtux/src/twitter_search.erl
+++ erlang/apps/hashtux/src/twitter_search.erl
@@ -8,36 +8,25 @@
 -define(ACCES_TOKEN, "3947085676-R5GaAeZAz2ns7wEcfZZ9Fw4Npt62kS4irnkgHcT").
 -define(ACCES_TOKEN_SECRET, "W1raFk3kqVEj38QFjDus2qCfk8IU8ilWgfTqAgk3T5Lv6").
 
-%% DEFAULT SEARCH: match all options
-search_hash_tag(HashTag, [{content_type, []}, {language, []}]) -> 
-    % Use oauth:sign/6 to generate a list of signed OAuth parameters, 
-    SignedParams = oauth:sign("GET", ?URL, [{q, HashTag}, {result_type, recent}], ?CONSUMER, ?ACCES_TOKEN, ?ACCES_TOKEN_SECRET),
-
-    % Send authorized GET request and get result as binary
-    Res = ibrowse:send_req(oauth:uri(?URL,SignedParams), [], get,[], [{response_format, binary}]),
-
-    {ok, Status, _ResponseHeaders, ResponseBody} = Res,
-
-    print_response_info(Status),
-
-    parse_response_body(HashTag, ResponseBody);
-
 % ADVANCED SEARCH: Filtered by Language and Type [text, image, video].
 search_hash_tag(HashTag, [{content_type, Types}, {language, Lang}]) -> 
+    
+    io:format("TWITTER ADVANCED SEARCH CALLED~n"),
 
     Count = 30,         %% MAX number of results returned by 'GET' request.
 
     % List of available Languages
-    LangParams = [en, es, fr, de, sv, bg, it, am],
+    LangParams = [<<"en">>, <<"es">>, <<"fr">>, <<"de">>, <<"sv">>, <<"bg">>, <<"it">>, <<"am">>],
 
     % Set API request parameters
     Options = case lists:member(Lang, LangParams) of
-        true -> [{q, HashTag}, {lang, Lang}, {count, Count}, {result_type, recent}];
-        false -> [{q, HashTag}, {result_type, recent}]                  %% 
+        true -> [{q, HashTag}, {lang, binary_to_atom(Lang, latin1)}, {count, Count}, {result_type, recent}];
+        false -> [{q, HashTag}, {result_type, recent}]                  %% do not set Language parameter in API request
     end,
 
+    io:format("Twitter: TYPES: ~p~n", [Types]),
     % Set content_type filter
-    TypeFilter = [atom_to_list(Z) || Z <- Types],    
+    TypeFilter = [binary_to_list(Z) || Z <- Types],    
 
     % Use oauth:sign/6 to generate a list of signed OAuth parameters, 
     SignedParams = oauth:sign("GET", ?URL, Options, ?CONSUMER, ?ACCES_TOKEN, ?ACCES_TOKEN_SECRET),
@@ -49,8 +38,10 @@ search_hash_tag(HashTag, [{content_type, Types}, {language, Lang}]) ->
 
     print_response_info(Status),
 
-    %% LangResult is a list of Internal JSX objects mined for a specified language
-    LangResult = parse_response_body(HashTag, ResponseBody),
+    %% Decode response body 
+    DecodedBody = jsx:decode(ResponseBody),
+    %% LangResult is a list of Internal JSX objects mined for a specified language (all languages if not specified)
+    LangResult = parser:parse_tweet_response_body(HashTag, DecodedBody),
 
     %% FILTER LangResult by content_type
     FilteredRes = [H || H <- LangResult, parser:is_content_type(H, TypeFilter)],
@@ -59,8 +50,11 @@ search_hash_tag(HashTag, [{content_type, Types}, {language, Lang}]) ->
     ResLength = length(FilteredRes),
     io:format("TWITTER ADVANCED SEARCH RETURNED ~p TWEETS~n", [ResLength]),
 
+    %% **** ADD CALL TO DB Here!!!*****
+    gen_server:call(db_serv, {add_doc, FilteredRes}),            %% Sends the document to DB
+
     % Return Filtered Result
-    FilteredRes.
+    FilteredRes.                                                 %% Return Result
 
 % Print request status information
 print_response_info(Status) ->
@@ -68,197 +62,3 @@ print_response_info(Status) ->
         "200" -> io:format("Request was fulfilled\n");
         _Other -> io:format("Got non-200 response\n")
     end.
-
-% Decode Response Body and parses result
-parse_response_body(HashTag, ResponseBody) ->
-    DecodedBody = jsx:decode(ResponseBody),
-
-    case extract(<<"search_metadata">>, DecodedBody) of
-        {found, SearchMetadata} -> 
-            % io:format("Search Metadata is ~p~n", [SearchMetadata]),
-            
-            case extract(<<"count">>, SearchMetadata) of
-                {found, SearchCount} -> io:format("TWITTER SIMPLE SEARCH RETURNED ~p TWEETS~n", [SearchCount]);
-                not_found -> io:format("Search Count NOT FOUND\n")
-            end;
-
-        not_found -> io:format("Search Metadata NOT FOUND\n")
-    end,
-
-    case extract(<<"statuses">>, DecodedBody) of
-        {found, StatusList} -> 
-            parse_status_list(HashTag, StatusList, []);                                
-        not_found -> []                                         %% return empty list if result was empty
-    end.
-
-    
-% the value of a key from decoded JSON message.
-extract(K, L) ->
-  case lists:keyfind(K, 1, L) of
-    {_, M} -> {found, M};
-    false  -> not_found
-  end.
-
-% Builds Profile URL for a given user screen_name
-build_profile_link(Screen_Name) -> 
-    A = lists:append("https://twitter.com/", binary_to_list(Screen_Name)),
-    list_to_binary(A).
-
-% Format Tags from JSON object format
-format_tags(Tags_List) -> 
-    format_tags(Tags_List, []).
-
-format_tags([], Result) -> Result;
-format_tags([H|T], Result) -> 
-    NewResult = Result ++ [get_single_tag(H)],
-    format_tags(T, NewResult).
-
-get_single_tag([{<<"text">>, A}, _]) -> A;
-get_single_tag([{_B, A}, _]) -> A.
-
-% Returns ONLY the first media element information (URL and Type).
-format_media_entity([]) -> {null, null};
-format_media_entity([H|_T]) -> format_single_media(H).          %% return info related to first media element
-
-format_single_media(Media) ->
-    Media_Url = case extract (<<"media_url">>, Media) of
-        {found, Url} -> Url;
-        not_found -> null
-    end,
-
-    Media_Type = case extract (<<"type">>, Media) of
-        {found, Type} -> 
-            case Type of
-                <<"photo">> -> <<"image">>;
-                _ -> <<"video">>
-            end;
-        not_found -> null
-    end,
-
-    {Media_Url, Media_Type}.
-
-% Remove empty fields from Tweet Result (i.e. removes fields that returned null)
-clean_result(L) -> [X || X <- L, has_null_value(X) == false].
-
-has_null_value({_, null}) -> true;
-has_null_value({_, _}) -> false.
-
-%% PARSING!!!!
-%% Calls parse_status_details on all statuses included in a given List
-%% Sends the document to DB and returns it
-parse_status_list(_HashTag, [], Result) -> 
-    gen_server:call(db_serv, {add_doc, Result}),            %% Sends the document to DB
-    Result;                                                 %% return doc
-parse_status_list(HashTag, [H|T], Result) -> 
-    NewResult = Result ++ [parse_status_details(HashTag, H)],
-    parse_status_list(HashTag, T, NewResult).
-
-%% Convert single Tweet Object to internal representation form
-parse_status_details(HashTag, Status) ->
-
-    %% Register the time the document was sent to DB
-    Timestamp = dateconv:get_timestamp(),
-
-    %% ======== Parsing Tweet details ==============
-    Tweet_ID = case extract(<<"id">>, Status) of
-        {found, X1} -> integer_to_binary(X1);
-        not_found -> null
-    end,
-
-    Date = case extract(<<"created_at">>, Status) of
-        {found, X2} -> dateconv:twitter_to_epoch(binary_to_list(X2));
-        not_found -> null
-    end,
-
-    Text = case extract(<<"text">>, Status) of
-        {found, X3} -> X3;
-        not_found -> null
-    end,
-
-    case extract(<<"metadata">>, Status) of
-        {found, MetaData} -> 
-            
-            Language = case extract(<<"iso_language_code">>, MetaData) of
-                {found, X4} -> X4;
-                not_found -> null
-            end;
-
-        not_found -> 
-            Language = null
-    end,
-
-    Coordinates = case extract(<<"coordinates">>, Status) of
-        {found, X5} -> X5;
-        not_found -> null
-    end,    
-
-    Retweet_Count = case extract(<<"retweet_count">>, Status) of
-        {found, X6} -> integer_to_binary(X6);
-        not_found -> null
-    end,
-
-    Favorited = case extract(<<"favorited">>, Status) of
-        {found, false} -> null;
-        {found, X7} -> X7;
-        not_found -> null
-    end,
-
-    %% Parse 'entities' node in Tweet Object
-    case extract(<<"entities">>, Status) of
-        {found, Entities} ->
-            Tags = case extract(<<"hashtags">>, Entities) of
-                {found, []} -> null;
-                {found, X8} -> format_tags(X8);
-                not_found -> null
-            end,
-
-            % Parse 'Media' entity
-            case extract(<<"media">>, Entities) of
-                {found, Media} -> 
-                    {Media_URL, Media_Type} = format_media_entity(Media);
-                not_found -> 
-                    Media_URL = null,
-                    Media_Type = <<"text">>
-            end;
-
-        %% ENTITIES not found
-        not_found -> 
-            Tags = null,
-            Media_URL = null,
-            Media_Type = <<"text">>
-    end,
-
-    %% ======== Parsing User details ==============
-
-    case extract(<<"user">>, Status) of
-        {found, UserInfo} -> 
-            
-            UserName = case extract(<<"name">>, UserInfo) of
-                {found, Y1} -> Y1;
-                not_found -> null
-            end,
-
-            ScreenName = case extract(<<"screen_name">>, UserInfo) of
-                {found, Y4} -> Y4;
-                not_found -> null
-            end, 
-
-            User_Profile_Link = case extract(<<"screen_name">>, UserInfo) of
-                {found, Y2} -> build_profile_link(Y2);
-                not_found -> null
-            end,
-
-            UserID = case extract(<<"id">>, UserInfo) of
-                {found, Y3} -> integer_to_binary(Y3);
-                not_found -> null
-            end;
-
-        not_found -> 
-            UserName = null,
-            ScreenName = null,
-            User_Profile_Link = null,
-            UserID = null
-    end,
-
-    A = [{<<"search_term">>, list_to_binary(HashTag)},{<<"service">>, <<"twitter">>}, {<<"service_id">>, Tweet_ID}, {<<"timestamp">>, Date}, {<<"insert_timestamp">>, Timestamp}, {<<"text">>, Text}, {<<"language">>, Language}, {<<"view_count">>, Retweet_Count}, {<<"likes">>, Favorited}, {<<"location">>, Coordinates}, {<<"tags">>, Tags}, {<<"resource_link_high">>, Media_URL}, {<<"resource_link_low">>, Media_URL}, {<<"content_type">>, Media_Type}, {<<"free_text_name">>, UserName}, {<<"username">>, ScreenName}, {<<"profile_link">>, User_Profile_Link}, {<<"user_id">>, UserID}],
-    clean_result(A).
