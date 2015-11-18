923fc267ef46019c9557b8eec27fb3f8e78d597b
TacoVox <jonas.kahler@icloud.com>
2015-11-18 15:36:13 +0100

    Applied the changes in connector and operations as well as update of the documentation

--- erlang/apps/hashtux/src/db_hash_writer.erl
+++ erlang/apps/hashtux/src/db_hash_writer.erl
@@ -1,90 +1,105 @@
-
 %% @author Jonas Kahler <jonas@derkahler.de> [www.derkahler.de]
 %% @author Niklas le Comte niklas.lecomte@hotmail.com [www.hashtux.com/niklas]
-%% %% %% @doc Initial database actions module
-%% %% %% @version 0.1
-
+%% @doc Initial database actions module
+%% @version 0.3
 %% -----------------------------------------------------------------------------
 %% | Sprint 1 // v0.1                                                          |
 %% | Created first as a normal module where the diffrent write operations was  |
 %% | functions. Then made it into a gen_server which handled a lot of calls.   |
 %% | It used every write operation defind in couch_operation.                  |
 %% -----------------------------------------------------------------------------
-%% -----------------------------------------------------------------------------
 %% | Sprint 2 // v0.2                                                          |
 %% | Changed how many operations to include because of changes in how data is  |
 %% | stored in the database. Only keept the operations which was needed for now|
 %% | add_doc, and delete_hash.                                                 |
 %% -----------------------------------------------------------------------------
-
+%% | Sprint 4 // v0.3                                                          |
+%% | Applied the new changes to this worker. It now fetches the credentials of |
+%% | the database server from the config file and uses them to write to the db |
+%% -----------------------------------------------------------------------------
 -module(db_hash_writer).
+-version(0.3).
 
 -behavior(gen_server).
 
--export([start_link/0, stop/0]).
+-export([start_link/0, stop/0, state/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2]).
+-export([handle_info/2, terminate/2]).
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
+%% Local database params
+-define(ADDR, fun() -> {ok, {ADDR, _, _}} =
+              application:get_env(db_conf, localdb), ADDR end).
+-define(USER, fun() -> {ok, {_, USER, _}} =
+              application:get_env(db_conf, localdb), USER end).
+-define(PASS, fun() -> {ok, {_, _, PASS}} =
+              application:get_env(db_conf, localdb), PASS end).
 
--define(DB, "hashtux/").
-
-%% Public API
-
-%% @doc Starts the server
+%% -----------------------------------------------------------------------------
+%% | Public API                                                                |
+%% -----------------------------------------------------------------------------
+%% @doc Starts the gen_server.
 start_link() ->
-	gen_server:start_link(?MODULE, [], []).
+    gen_server:start_link(?MODULE, [], []).
 
-%% @doc Stops the server
+%% @doc Stops the gen_server
 stop(Module) ->
-	gen_server:call(Module, stop).
-
+    gen_server:call(Module, stop).
 stop() ->
-	stop(self()).
+    stop(self()).
 
+%% @doc Returns the current state of the module
 state(Module) ->
-	gen_server:call(Module, state).
-
+    gen_server:call(Module, state).
 state() ->
-	state(self()).
+    state(self()).
 
-%% Server implementation, a.k.a.: callbacks
-
+%% -----------------------------------------------------------------------------
+%% | Server implementation                                                     |
+%% -----------------------------------------------------------------------------
+%% @doc Init function which starts the server with an empty state.
 init([]) ->
-	{ok, []}.
+    {ok, []}.
 
-%% @doc Handles the calls
+%% @doc Call to stop the server.
 handle_call(stop, _From, _State) ->
-	{stop, normal, stopped, _State};
-
-%% @doc Catches all cases except stop
+    {stop, normal, stopped, _State};
+%% @doc Other calls to the server are not supported.
 handle_call(_, _, _) ->
-	error(badarth).
+    error(undef).
 
-%% %% @doc These handels the messages coming in and do the operations
-
+%% @doc Add a document to the database
 handle_cast({add_doc, Content, Rec}, State) ->
-        add_docs(Content),
-	Rec ! {self(), true},	
-	{stop, normal, State};
-
-%% Rewrite this later!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    add_docs(Content),
+    Rec ! {self(), true},
+    {stop, normal, State};
+%% Deletes a hashtag from the database
 handle_cast({delete_hash, Hashtag, Rec}, State) ->
-	Results = couch_operations:doc_get(?DB ++ "_design/post/_view/by_hashtag?key=\"" ++ Hashtag ++ "\""),
-	{_, Posts} = lists:keyfind(<<"rows">>, 1, Results),
-	[couch_operations:doc_delete(?DB ++ binary_to_list(ID), binary_to_list(Rev)) ||
-	    [{<<"_id">>, ID}, {<<"_rev">>, Rev} | _] <- [Content || {<<"value">>, Content} <- lists:flatten(Posts)]],
-        Rec ! {self(), true},
-	{stop, normal, State}.
+    Results = couch_operations:doc_get({?ADDR() ++
+              "hashtux/_design/post/_view/by_hashtag?key=\"" ++
+              Hashtag ++ "\"", ?USER(), ?PASS()}),
+    {_, Posts} = lists:keyfind(<<"rows">>, 1, Results),
+    [couch_operations:doc_delete({?ADDR() ++ binary_to_list(ID),
+       binary_to_list(Rev), ?USER(), ?PASS()}) ||
+         [{<<"_id">>, ID}, {<<"_rev">>, Rev} | _] <- [Content ||
+       {<<"value">>, Content} <- lists:flatten(Posts)]],
+    Rec ! {self(), true},
+    {stop, normal, State}.
 
-%% @doc Handles Info (not used) 
+%% @doc Normal messages to the server are not supported.
 handle_info(_Info, _State) ->
-	{noreply, _State}.
+    error(undef).
 
-%% @doc Terminates the server
-terminate(_Reason, _State) -> 
-  	ok.
+%% @doc Terminates the gen_server
+terminate(_Reason, _State) ->
+    ok.
 
+%% -----------------------------------------------------------------------------
+%% | Helperfunction implementation                                             |
+%% -----------------------------------------------------------------------------
+%% @doc Recursive unction to add several documents at a time to the database.
 add_docs([]) -> ok;
 add_docs([H|T]) ->
-	UUID = couch_operations:get_uuid(),
-	couch_operations:doc_add([?DB | [UUID]], H),
-	add_docs(T).
+    UUID = couch_operations:get_uuid(),
+    couch_operations:doc_add({?ADDR() ++ "hashtux/" ++ UUID,
+                              ?USER(), ?PASS()}, H),
+    add_docs(T).
--- erlang/apps/hashtux/src/db_userstats_reader.erl
+++ erlang/apps/hashtux/src/db_userstats_reader.erl
@@ -1,7 +1,7 @@
+%% @author Jonas Kahler <jonas.kahler@icloud.com> [www.derkahler.de]
 %% @author Niklas le Comte niklas.lecomte@hotmail.com [www.hashtux.com/niklas]
-%% %% %% @doc A worker that handles the reading from the db for the userstats
-%% %% %% @version 0.1
-
+%% @doc A worker that handles the reading from the db for the userstats
+%% @version 0.1
 %% -----------------------------------------------------------------------------
 %% | Sprint 4                                                                  |
 %% | Version 0.1                                                               |
@@ -10,110 +10,148 @@
 %% | it can read from the db and use mapreduce functions for getting the corre-|
 %% | ct data.                                                                  |
 %% -----------------------------------------------------------------------------
-
 -module(db_userstats_reader).
+-version(0.1).
 
--behaviour(gen_server).
+-behavior(gen_server).
 
 -export([start_link/0, stop/0, state/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2]).
+-export([code_change/3, terminate/2]).
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
+%% Local database params
+-define(ADDR, fun() -> {ok, {ADDR, _, _}} =
+              application:get_env(db_conf, localdb), ADDR end).
+-define(USER, fun() -> {ok, {_, USER, _}} =
+              application:get_env(db_conf, localdb), USER end).
+-define(PASS, fun() -> {ok, {_, _, PASS}} =
+              application:get_env(db_conf, localdb), PASS end).
 
--define(DB, "hashtux_userstats/").
-
-%% Public API
-
-%% @doc Starts the server
+%% -----------------------------------------------------------------------------
+%% | Public API                                                                |
+%% -----------------------------------------------------------------------------
+%% @doc Starts the gen_server and links it to the calling process.
 start_link() ->
-	gen_server:start_link(?MODULE, [], []).
+    gen_server:start_link(?MODULE, [], []).
 
-%% @doc Calls a stop tp the server
+%% @doc Stops the gen_server.
 stop(Module) ->
-	gen_server:call(Module, stop).
-
+    gen_server:call(Module, stop).
 stop() ->
-	stop(self()).
+    stop(self()).
 
+%% @doc Returns the state of the gen_sever
 state(Module) ->
-	gen_server:call(Module, state).
-
+    gen_server:call(Module, state).
 state() ->
-	state(self()).
+    state(self()).
 
-%% Server implementation, a.k.a.: callbacks
-
+%% -----------------------------------------------------------------------------
+%% | Server implementation                                                     |
+%% -----------------------------------------------------------------------------
+%% @doc Init function which starts the server with an empty state.
 init([]) ->
-	{ok, []}.
+    {ok, []}.
 
-%% @doc Handles the calls
+%% @doc Stop call to the server.
 handle_call(stop, _From, _State) ->
-	{stop, normal, stopped, _State};
-
-%% @doc Catches all calls
+    {stop, normal, stopped, _State};
+%% @doc Other calls are not supported.
 handle_call(_, _, _) ->
-	error(badarth).
+    error(undef).
 
-%% %% @doc Handels the cast which is the messages where we doing operations on.
+%% @doc Get statistics based on the search term from the server.
 handle_cast({get_stats, get_search_term, Options, Rec}, State) ->
-	R = couch_operations:doc_get_mapreduce_cont(?DB ++ "_design/stat/_view/by_search_term?" ++	
-			   			    db_options_handler:pre_search_opt(db_options_handler:order_options(Options)) ++ "&group=true"),
-	Result = db_filter:order_by_value(db_filter:group_by_subkey(R)),
-	Opt = db_options_handler:order_options(Options),
-	LR = db_options_handler:search_opt([{limit, Limit} || {Atom, Limit}  <- Opt, {Atom, Limit} == {limit, Limit}], Result),
-	Rec ! {self(), LR},
-	{stop, normal, State};
+    R = couch_operations:doc_get_mapreduce_cont({?ADDR() ++
+             "hashtux_userstats/_design/stat/_view/by_search_term?" ++
+             db_options_handler:pre_search_opt(
+             db_options_handler:order_options(Options)) ++ "&group=true",
+             ?USER(), ?PASS()}),
+    Result = db_filter:order_by_value(db_filter:group_by_subkey(R)),
+    Opt = db_options_handler:order_options(Options),
+    LR = db_options_handler:search_opt([{limit, Limit} || {Atom, Limit}  <- Opt,
+                                      {Atom, Limit} == {limit, Limit}], Result),
+    Rec ! {self(), LR},
+    {stop, normal, State};
 
+%% @doc Get statistics based on the browserfrom the server.
 handle_cast({get_stats, get_browser, Options, Rec}, State) ->
-	R = couch_operations:doc_get_mapreduce_cont(?DB ++ "_design/stat/_view/by_browser?" ++
-						    db_options_handler:pre_search_opt(db_options_handler:order_options(Options)) ++ "&group=true"),
-	Result = db_filter:order_by_value(db_filter:group_by_subkey(R)),
-	Opt = db_options_handler:order_options(Options),
-	LR = db_options_handler:search_opt([{limit, Limit} || {Atom, Limit}  <- Opt, {Atom, Limit} == {limit, Limit}], Result),
-	Rec ! {self(), LR},
-	{stop, normal, State};
+    R = couch_operations:doc_get_mapreduce_cont({?ADDR ++
+             "hashtux_userstats/_design/stat/_view/by_browser?" ++
+             db_options_handler:pre_search_opt(
+             db_options_handler:order_options(Options)) ++ "&group=true",
+             ?USER(), ?PASS()}),
+    Result = db_filter:order_by_value(db_filter:group_by_subkey(R)),
+    Opt = db_options_handler:order_options(Options),
+    LR = db_options_handler:search_opt([{limit, Limit} || {Atom, Limit}  <- Opt,
+                                      {Atom, Limit} == {limit, Limit}], Result),
+    Rec ! {self(), LR},
+    {stop, normal, State};
 
-handle_cast({get_stats, get_language, Options, Rec}, State) -> 
-	R = couch_operations:doc_get_mapreduce_cont(?DB ++ "_design/stat/_view/by_language?" ++  
-						    db_options_handler:pre_search_opt(db_options_handler:order_options(Options)) ++ "&group=true"),
-	Result = db_filter:order_by_value(db_filter:group_by_subkey(R)),
-	Opt = db_options_handler:order_options(Options),
-	LR = db_options_handler:search_opt([{limit, Limit} || {Atom, Limit}  <- Opt, {Atom, Limit} == {limit, Limit}], Result),
-	Rec ! {self(), Result},
-	{stop, normal, State};
+%% @doc Get statistics based the language on from the server.
+handle_cast({get_stats, get_language, Options, Rec}, State) ->
+    R = couch_operations:doc_get_mapreduce_cont({?ADDR() ++
+             "hashtux_userstats/_design/stat/_view/by_language?" ++
+             db_options_handler:pre_search_opt(
+             db_options_handler:order_options(Options)) ++ "&group=true",
+             ?USER(), ?PASS()}),
+    Result = db_filter:order_by_value(db_filter:group_by_subkey(R)),
+    Opt = db_options_handler:order_options(Options),
+    LR = db_options_handler:search_opt([{limit, Limit} || {Atom, Limit}  <- Opt,
+                                      {Atom, Limit} == {limit, Limit}], Result),
+    Rec ! {self(), Result},
+    {stop, normal, State};
 
+%% @doc Get statistics based on the platform from the server.
 handle_cast({get_stats, get_platform, Options, Rec}, State) ->
-	R = couch_operations:doc_get_mapreduce_cont(?DB ++ "_design/stat/_view/by_platform?" ++
-						   db_options_handler:pre_search_opt(db_options_handler:order_options(Options)) ++ "&group=true"),
-	Result = db_filter:order_by_value(db_filter:group_by_subkey(R)),
-	Opt = db_options_handler:order_options(Options),
-	LR = db_options_handler:search_opt([{limit, Limit} || {Atom, Limit}  <- Opt, {Atom, Limit} == {limit, Limit}], Result),
-	Rec ! {self(), Result},
-	{stop, normal, State};
+    R = couch_operations:doc_get_mapreduce_cont({?ADDR() ++
+             "hashtux_userstats/_design/stat/_view/by_platform?" ++
+             db_options_handler:pre_search_opt(
+             db_options_handler:order_options(Options)) ++ "&group=true",
+             ?USER(), ?PASS()}),
+    Result = db_filter:order_by_value(db_filter:group_by_subkey(R)),
+    Opt = db_options_handler:order_options(Options),
+    LR = db_options_handler:search_opt([{limit, Limit} || {Atom, Limit}  <- Opt,
+                                      {Atom, Limit} == {limit, Limit}], Result),
+    Rec ! {self(), Result},
+    {stop, normal, State};
 
+%% @doc Get statistics based on the browser version from the server.
 handle_cast({get_stats, get_browser_version, Options, Rec}, State) ->
-	R = couch_operations:doc_get_mapreduce_cont(?DB ++ "_design/stat/_view/by_browser_version?" ++ 
-						   db_options_handler:pre_search_opt(db_options_handler:order_options(Options)) ++ "&group=true"),
-	Result = db_filter:order_by_value(db_filter:group_by_subkey(R)),
-	Opt = db_options_handler:order_options(Options),
-	LR = db_options_handler:search_opt([{limit, Limit} || {Atom, Limit}  <- Opt, {Atom, Limit} == {limit, Limit}], Result),
-	Rec ! {self(), Result},
-	{stop, normal, State};
+    R = couch_operations:doc_get_mapreduce_cont({?ADDR() ++
+             "hashtux_userstats/_design/stat/_view/by_browser_version?" ++
+             db_options_handler:pre_search_opt(
+             db_options_handler:order_options(Options)) ++ "&group=true",
+             ?USER(), ?PASS()}),
+    Result = db_filter:order_by_value(db_filter:group_by_subkey(R)),
+    Opt = db_options_handler:order_options(Options),
+    LR = db_options_handler:search_opt([{limit, Limit} || {Atom, Limit}  <- Opt,
+                                      {Atom, Limit} == {limit, Limit}], Result),
+    Rec ! {self(), Result},
+    {stop, normal, State};
 
+%% @doc Get statistics based on the platform/brower from the server.
 handle_cast({get_stats, get_platform_browser, Options, Rec}, State) ->
-	R = couch_operations:doc_get_mapreduce_cont(?DB ++ "_design/stat/_view/by_platform_browser?" ++ 
-						   db_options_handler:pre_search_opt(db_options_handler:order_options(Options)) ++ "&group=true"),
-	Result = db_filter:order_by_value(db_filter:group_by_subkey(R)),
-	Opt = db_options_handler:order_options(Options),
-	LR = db_options_handler:search_opt([{limit, Limit} || {Atom, Limit}  <- Opt, {Atom, Limit} == {limit, Limit}], Result),
-	Rec ! {self(), Result},
-	{stop, normal, State}.
+    R = couch_operations:doc_get_mapreduce_cont({?ADDR() ++
+             "hashtux_userstats/_design/stat/_view/by_platform_browser?" ++
+             db_options_handler:pre_search_opt(
+             db_options_handler:order_options(Options)) ++ "&group=true",
+             ?USER(), ?PASS()}),
+    Result = db_filter:order_by_value(db_filter:group_by_subkey(R)),
+    Opt = db_options_handler:order_options(Options),
+    LR = db_options_handler:search_opt([{limit, Limit} || {Atom, Limit}  <- Opt,
+                                      {Atom, Limit} == {limit, Limit}], Result),
+    Rec ! {self(), Result},
+    {stop, normal, State}.
 
-
-%% @doc Handels the info (not used)
+%% @doc Normal messages to the server are not supported.
 handle_info(_Info, _State) ->
-	{noreply, _State}.
+    error(undef).
+
+%% doc Code update
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
 
 %% @doc Terminates the server
 terminate(_Reason, _State) ->
-	ok.
-
+    ok.
