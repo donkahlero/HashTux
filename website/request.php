f94c56a3878af63827b351f490a0e2fe9bf3fb09
Tville <jerker@soundandvision.se>
2015-11-18 16:02:52 +0100

    D C D C A D B C A D B A

--- website/request.php
+++ website/request.php
@@ -16,9 +16,15 @@ require_once 'lib/UserAgentParser.php';
 /*
  * Makes a request to the erlang backend and returns the result (as JSON).
  * $search: the search term
- * $request_data: options and user habit data. Use build_request_data() to generate.	  
+ * $options: associative array with options. Use build_request_options for convenience.  
  */
-function request($search, $request_data) { 
+function request($term, $options) {
+	// Filter out any null values in the options
+	$options = array_filter($options);
+	
+	// Put together the request body in the [options, user habit data] format
+	$habit_data = get_user_habit_data();
+	$request_body = json_encode([$options, $habit_data]);
 	
 	// Start curl if the module is installed, otherwise stop execution
 	if (!function_exists('curl_init')){
@@ -31,10 +37,10 @@ function request($search, $request_data) {
     
     // Tell cURL we want to POST the JSON in $request_data
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $request_data);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $request_body);
     
 	// Set the URL, including urlencoded (helps if there are special characters) search term
-    curl_setopt ($ch, CURLOPT_URL, "http://localhost:8080/" . urlencode($search));
+    curl_setopt ($ch, CURLOPT_URL, "http://localhost:8080/" . urlencode(clean($term)));
     
 	// Execute the request
 	$output = curl_exec($ch);  
@@ -44,7 +50,7 @@ function request($search, $request_data) {
 
 
 /*
- * Builds the JSON representation of all the options and data that needs to be passed
+ * Builds an associative array of all the options that need to be passed
  * to the erlang server to make a request. 
  * 
  * $request_type: "search" or "update" - use update from ajax or other subsequent requests.
@@ -52,18 +58,11 @@ function request($search, $request_data) {
  * $content_type: array of strings, ex ["images", "videos"]. empty list for all.
  * $language specified as a two character string
  *
- * Format of the resulting object will be [OPTIONS, USER_HABIT_DATA] where
- * OPTIONS and USER_HABIT_DATA each are lists of key-value tuples.
- *
+ * Unspecified fields are set to null by default and then omitted in the
+ * resulting associative array
  */
-function build_request_data($request_type = "search", $services = [],
-		$content_type = [], $language = "en") {
-
-	// Parse the user agent string so we get the components we need
-	$user_agent_strings = parse_user_agent();
-	$browser = $user_agent_strings['browser'];
-	$browser_version = $user_agent_strings['version'];
-	$platform = $user_agent_strings['platform'];
+function build_request_options($request_type = "search", $services = null,
+		$content_type = null, $language = null) {
 
 	// Set up the parameter array
 	$options = array(
@@ -72,6 +71,23 @@ function build_request_data($request_type = "search", $services = [],
 			'content_type' => $content_type,
 			'language' => $language,
 	);
+	// Filter out any null values so they're not even sent
+	$options = array_filter($options);
+       
+}
+
+
+/*
+ * Put together the user habit data associative array that will later be part
+ * of the [options, user habit data] JSON object sent to the erlang backend
+ */
+function get_user_habit_data() {
+	// Parse the user agent string so we get the components we need
+	$user_agent_strings = parse_user_agent();
+	$browser = $user_agent_strings['browser'];
+	$browser_version = $user_agent_strings['version'];
+	$platform = $user_agent_strings['platform'];
+	
 	$user_habit_data = array(
 			'timestamp' => time(),
 			'session_id' => session_id(),
@@ -79,43 +95,16 @@ function build_request_data($request_type = "search", $services = [],
 			'browser' => $browser,
 			'browser_version' => $browser_version,
 			'platform' => $platform,
-				
+	
 	);
-
-	$options = distinguish_tuples($options);
-	$user_habit_data = distinguish_tuples($user_habit_data);
-
-	return json_encode([$options, $user_habit_data]);
+	return $user_habit_data;
 }
 
 
 /*
- * Converts a PHP associative key-value array into an array with key-value
- * objects that is as close as possible to Erlangs "list of tuples".
- *
- * Where an associative array in PHP would be encoded in JSON as
- * {"key1" : "value1", "key2" : "value2"}
- *
- * we instead get
- *
- * [{"key1" : "value1"}, {"key2" : "value2"}]
- *
- * if we encode the output of this method.
- *
- * (Because PHP doesn't really have tuples, the easiest way to convert it reasonably
- * is to use an associative array in PHP... FOR EACH key-value pair! Since we need 
- * one ass. array for each, i just use a two-dimensional one where the first isn't
- * associative (just uses numbers), hence we end up with "a list of tuples"
- * if we convert this outer array to json.)
- * http://php.net/manual/en/function.json-encode.php
+ * Remove special characters from string using regex. 
  */
-function distinguish_tuples($array) {
-	$tuples = [];
-	foreach ($array as $key => $value) {
-		// Add each value with the corresponding key as the only element
-		// for that index in the leftmost array []
-		$tuples[sizeof($tuples)][$key] = $value;
-	}
-	return $tuples;
+function clean($string) {
+   return preg_replace('/[^A-Öa-ö1-9\-\_\+ ]/', '', $string); // Removes special chars.
 }
 ?>	
