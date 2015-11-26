681b72b2670dd757994ed015c848012278b9ed01
Dennis <realdkk@gmail.com>
2015-11-26 13:00:26 +0100

    Made two new css files, one for all personal classes used in index.php and search.php and one css file for stats.php, just to simplify the working environment (Dennis).

--- website/search.php
+++ website/search.php
@@ -16,6 +16,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/hashtux.css"rel="stylesheet">
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
