77b97495f95339e7f97fac0f26cab28b4bde5949
Jerker <jerker@soundandvision.se>
2015-11-30 16:00:23 +0100

    Fixed broken include paths

--- website/ajax.php
+++ website/ajax.php
@@ -13,7 +13,7 @@
 	/* Start a new session or resume if the client has a cookie ;) */
 	session_start();
 	require_once ("include/request.php");
-	
+
 	/* See if request type is specified, otherwise assume update */
 	if (isset($_GET['request_type'])) {
 		$request_type = $_GET['request_type'];
