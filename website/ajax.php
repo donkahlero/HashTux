b022b682b996b32cedaa76ad8a86a84721c487ef
Jerker <jerker@soundandvision.se>
2015-11-30 15:55:00 +0100

    Folder structure for website

--- website/ajax.php
+++ website/ajax.php
@@ -12,7 +12,7 @@
 
 	/* Start a new session or resume if the client has a cookie ;) */
 	session_start();
-	require_once ("request.php");
+	require_once ("include/request.php");
 	
 	/* See if request type is specified, otherwise assume update */
 	if (isset($_GET['request_type'])) {
