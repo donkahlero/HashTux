e4b4a8519b7767e504b49bc23280eee93427fb5b
Jerker <Jerker@soundandvision.se>
2015-12-09 14:13:50 +0100

    Port for erlang backend now is configurable and works

--- erlang/apps/hashtux/src/http_cowboy.erl
+++ erlang/apps/hashtux/src/http_cowboy.erl
@@ -10,8 +10,7 @@
 %% @doc Start the cowboy HTTP server, handler is defined in http_handler.erl.
 start() ->
 	%% Check the configuration for which port to listen to
-	{ok, ListenerConf} = application:get_env(hashtux, instagram_account),
-	Port = aux_functions:get_value(listen_port, ListenerConf),
+	{ok, Port} = application:get_env(listener_conf, listen_port),
 
 	%% Start the http listener	
 	Dispatch = cowboy_router:compile([
@@ -20,4 +19,4 @@ start() ->
     cowboy:start_http(my_http_listener, 100, [{port, Port}],
         [{env, [{dispatch, Dispatch}]}]
     ),
-	io:format("~n~nhashtux_app: Started the cowboy http_handler.~n~n", []).
+	io:format("~n~nhashtux_app: Started the cowboy http_handler at port ~p.~n~n", [Port]).
