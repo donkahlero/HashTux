f1e4d15c0e91e55c0a8f9497edd85adce4298401
tville <jerker@soundandvision.se>
2015-12-09 13:49:03 +0100

    Port is now configurable

--- erlang/apps/hashtux/src/hashtux_app.erl
+++ erlang/apps/hashtux/src/hashtux_app.erl
@@ -17,12 +17,18 @@
 
 start(_StartType, _StartArgs) ->
 	%%
-	%% Start the http listener
+	%% Bring up the Cowboy HTTP listener. Handler is in http_handler.erl.
 	%%
+
+	%% Check the configuration for which port to listen to
+	{ok, ListenerConf} = application:get_env(hashtux, instagram_account),
+	Port = aux_functions:get_value(listen_port, ListenerConf),
+
+	%% Start the http listener	
 	Dispatch = cowboy_router:compile([
         {'_', [{'_', http_handler, []}]}
     ]),
-    cowboy:start_http(my_http_listener, 100, [{port, 8080}],
+    cowboy:start_http(my_http_listener, 100, [{port, Port}],
         [{env, [{dispatch, Dispatch}]}]
     ),
 	io:format("~n~nhashtux_app: Started the cowboy http_handler.~n~n", []),
