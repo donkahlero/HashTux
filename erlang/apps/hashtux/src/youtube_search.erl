20ce2184f0ea9cef58c79b21952effe41018fdbb
ThreePhase3fo <marco.trifance@gmail.com>
2015-12-14 12:51:55 +0100

    Commented modules apis_aux, parser, twitter_search, youtube_search

--- erlang/apps/hashtux/src/youtube_search.erl
+++ erlang/apps/hashtux/src/youtube_search.erl
@@ -1,12 +1,15 @@
+%% @author Marco Trifance <marco.trifance@gmail.com>
+%% @doc Handle Youtube Data API search requests according to HashTux filters and parameters.
+
 -module(youtube_search).
 
 -export([search/2]).
 
--define(SEARCH_URL, "https://www.googleapis.com/youtube/v3/search?").		% Endpoint for a Search:list request to Youtube Data API
--define(VIDEOS_URL, "https://www.googleapis.com/youtube/v3/videos?").		% Endpoint for a Videos:list request to Youtube Data API
+%% Endpoint for a Search:list request to Youtube Data API
+-define(SEARCH_URL, "https://www.googleapis.com/youtube/v3/search?").		
+%% Endpoint for a Videos:list request to Youtube Data API
+-define(VIDEOS_URL, "https://www.googleapis.com/youtube/v3/videos?").		
 
-
-%%
 %% @doc     Sends a GET request to the 'Youtube Data API'.
 %% @return  A list of two tuples in the following format: [{filtered, FilteredRes}, {unfiltered, LangResult}].
 %%          UnfilteredRes: a list of youtube videos in internal representation form matching the specified values
@@ -15,7 +18,6 @@
 %% @params  Types: a list of types [text, image, video] of feeds requested by the client.
 %% @params  Lang: the langauge of the feeds the query will request
 %% @HistoryTimestamp: the publication date of the feeds the query will request (epoch timestamp)
-%%
 search(HashTag, [{content_type, Types}, {language, Language}, {history_timestamp, HistoryTimestamp}]) ->
 
 	% True if client filtered by content type and requested videos 
@@ -67,19 +69,17 @@ query_youtube_API(HashTag, Count, HistoryTimestamp) ->
 
 	MaxResults = "maxResults=" ++ integer_to_list(Count),					%% increase max results to 10
 
-	%%(CHANGE THIS TO ONE DAY) ==============
 	% Handle case history_timestamp
 	After = case HistoryTimestamp of
-		%% Generate AFTER Time Parameter for normal search
+		%% Generate AFTER Time Parameter for normal search. Fetch only videos uploaded in the last 24-hours
 		[] -> apis_aux:youtube_get_after_param();
 		%% Generate BEFORE and AFTER time parameters for History search
 		Timestamp -> apis_aux:youtube_get_after_before_params(Timestamp)
 	end,
-	%% =====================================
 
 	Type = "type=video&videoCaption=closedCaption&videoEmbeddable=true",	%% filter only VIDEO 'resource type' that contain capion
 
-	Key = "key=" ++ aux_functions:get_youtube_keys(),									%% API KEY parameter
+	Key = "key=" ++ aux_functions:get_youtube_keys(),						%% API KEY parameter
 
 	Url = ?SEARCH_URL ++ Part ++ "&" ++ Q ++ "&" ++ MaxResults ++ "&" ++ After ++ "&" ++ Type ++ "&" ++ Key,
 
@@ -93,7 +93,7 @@ query_youtube_API(HashTag, Count, HistoryTimestamp) ->
     			{found, Ids} -> 
     				CleanIds = parser:extract_youtube_ids(Ids),
     				VideoList = [video_search(X) || X <- CleanIds],									% GET a list of decoded Video 'resources' 
-    				Results = [parser:parse_youtube_video(Y, HashTag) || Y <- VideoList],			% ***return a list of parsed video items (*** SEND to DB!!!)
+    				Results = [parser:parse_youtube_video(Y, HashTag) || Y <- VideoList],			% Return a list of parsed video items 
     				ResLength = length(Results),
     				io:format("YOUTUBE API Query RETURNED ~p RESULTS~n", [ResLength]),
     				Results;																		% return Youtube results
