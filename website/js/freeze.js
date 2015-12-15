f2dcc69e0404342b997493c45179dc6a7439c321
DKarlberg <realdkk@gmail.com>
2015-12-15 12:55:20 +0100

    Freeze bug fixed. Close button added to the optionsmenu. Tooltips on the optionsmenu now change on click. Changes on the statistics end. (Aman)

--- website/js/freeze.js
+++ website/js/freeze.js
@@ -21,28 +21,27 @@ function screenFreeze() {
 // using the
 // boolean frozen and setting it to true.
 function freeze() {
-	for (i = 0; i < displayed.length; i++) {
-		displayed[i].frozen = true;
+    for (i = 0; i < displayed.length; i++) {
+        displayed[i].frozen = true;
 
-		$('#' + displayed[i].tile + "freeze").css('opacity', '1');
+        $('#' + displayed[i].tile + "freeze").css('opacity', '1');
+    }
 
-	}
-
-	$('#freezeBtn').css('opacity', '1');
-	screenFrozen = true;
+    $('#freezeBtn').css('opacity', '1');
+    screenFrozen = true;
 }
 // unfreeze is used to check for a displayed tiles and unpause it if its paused
 // using the
 // boolean frozen and setting it to false.
 function unfreeze() {
-	for (i = 0; i < displayed.length; i++) {
-		displayed[i].frozen = false;
+    for (i = 0; i < displayed.length; i++) {
+        displayed[i].frozen = false;
 
-		$('#' + displayed[i].tile + "freeze").css('opacity', '0.5');
-	}
+        $('#' + displayed[i].tile + "freeze").css('opacity', '0.5');
+    }
 
-	$('#freezeBtn').css('opacity', '0.35');
-	screenFrozen = false;
+    $('#freezeBtn').css('opacity', '0.35');
+    screenFrozen = false;
 }
 
 // tileFreeze works in a similar manner of freeze and unfreeze the difference is
@@ -51,63 +50,56 @@ function unfreeze() {
 // freezes the tile.
 function tileFreeze() {
 
-	var tile = arguments[0].id;
+    var tile = arguments[0].id;
 
-	for (i = 0; i < displayed.length; i++) {
-		if (displayed[i].tile === tile && !displayed[i].frozen) {
-			displayed[i].frozen = true;
+    for (i = 0; i < displayed.length; i++) {
+        if (displayed[i].tile === tile && !displayed[i].frozen) {
+            displayed[i].frozen = true;
 
-			$('#' + tile + "freeze").css('opacity', '1');
-		}
+            $('#' + tile + "freeze").css('opacity', '1');
+        }
 
-		else if (displayed[i].tile === tile && displayed[i].frozen) {
-			displayed[i].frozen = false;
+        else if (displayed[i].tile === tile && displayed[i].frozen) {
+            displayed[i].frozen = false;
 
-			$('#' + tile + "freeze").css('opacity', '0.5');
-		}
-	}
+            $('#' + tile + "freeze").css('opacity', '0.5');
+        }
+    }
 }
 
 // Handles a keyboard input in this case a space.
 // When pressed it trigger screenFreeze() and togglePlay();
 $(document).keypress(function(e) {
-	if ((e.which && e.which == 32) || (e.keyCode && e.keyCode == 32)) {
-		togglePlay();
-		screenFreeze();
-		return false;
-	}
+    if ((e.which && e.which == 32) || (e.keyCode && e.keyCode == 32)) {
+        togglePlay();
+        screenFreeze();
+        return false;
+    }
 
-	else {
-		return true;
-	}
-});
-
-$('').click(function() { // Extra onclick function incase we decide to use a
-							// button.
-	togglePlay();
-	screenFreeze();
-	return false;
+    else {
+        return true;
+    }
 });
 
 // This function animates the play and pause pictures
 // The two pictures fade in and out.
 function togglePlay() {
-	var $elem = $('#player').children(':first');
-	$elem.stop().show().animate({
-		'marginTop' : '-175px',
-		'marginLeft' : '-175px',
-		'width' : '350px',
-		'height' : '350px',
-		'opacity' : '0'
-	}, function() {
-		$(this).css({
-			'width' : '100px',
-			'height' : '100px',
-			'margin-left' : '-50px',
-			'margin-top' : '-50px',
-			'opacity' : '1',
-			'display' : 'none'
-		});
-	});
-	$elem.parent().append($elem);
+    var $elem = $('#player').children(':first');
+    $elem.stop().show().animate({
+        'marginTop' : '-175px',
+        'marginLeft' : '-175px',
+        'width' : '350px',
+        'height' : '350px',
+        'opacity' : '0'
+    }, function() {
+        $(this).css({
+            'width' : '100px',
+            'height' : '100px',
+            'margin-left' : '-50px',
+            'margin-top' : '-50px',
+            'opacity' : '1',
+            'display' : 'none'
+        });
+    });
+    $elem.parent().append($elem);
 }
