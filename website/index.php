9ec494bdab85c603b67f9ce853aba8f34c2decff
DKarlberg <realdkk@gmail.com>
2015-12-17 12:19:51 +0100

    Fixed a couple of icons and a design bug on index.php.

--- website/index.php
+++ website/index.php
@@ -10,13 +10,12 @@
     
     <title>HashTux</title>
     
-    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/hashtux.css" rel="stylesheet">
     
     <link href="images/favicon.ico" rel="shortcut icon">
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
     <script src="js/frontpagegrid.js"></script>
     <script src="js/general.js"></script>    
     <script src="js/popular.js"></script>    
